3f67f131b8983c89d585408b7c8e58c3d869391b
Neekon <neekon91@gmail.com>
2016-08-29 16:56:55 -0700

    heroku

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+// module.exports = function (grunt) {
+//   grunt.initConfig({
+//     pkg: grunt.file.readJSON('package.json'),
+//     concat: {
+//       css: {
+//         src: ['bootstrap/bootstrap-3.3.7-dist/css/bootstrap.min.css'],
+//         dest: 'dist/css/styles.css',
+//       },
+//       js: {
+//         src: [
+//           'server/server.js',
+//         ],
+//         dest: 'dist/js/scripts.js',
+//       },
+//     },
+//     uglify: {
+//       options: {
+//         banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n',
+//       },
+//       build: {
+//         files: {
+//           'dist/js/server.min.js': 'dist/js/server.js',
+//         },
+//       },
+//     },
+//     cssmin: {
+//       build: {
+//         files: {
+//           'dist/css/style.min.css': 'dist/css/styles.css',
+//         },
+//       },
+//     },
+    
+// };
+// working progress
+
+
---
+++ server/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/../'));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 5000;
+
+app.listen(port);
+console.log("Server is doing big things on port 5000");
