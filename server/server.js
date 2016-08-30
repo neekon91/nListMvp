dc33f9d31cd6e78a6d21ef21be7fb4435a8dd926
Neekon <neekon91@gmail.com>
2016-08-30 10:59:21 -0700

    second commit

--- server/server.js
+++ server/server.js
@@ -6,6 +6,6 @@ app.use(express.static(__dirname + '/../'));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 5000;
-
+ 
 app.listen(port);
 console.log("Server is doing big things on port 5000");
